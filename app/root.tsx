01130c57cbfce297f0818232fea27d6a3b3efe69
Jack Clackett <jack@noquarter.co>
2025-06-25 16:27:20 +0200

    default font

--- app/root.tsx
+++ app/root.tsx
@@ -7,19 +7,6 @@ import { Toaster } from "sonner"
 import type { Route } from "./+types/root"
 import "./globals.css"
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-]
-
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL)
 const convexQueryClient = new ConvexQueryClient(convex)
 const queryClient = new QueryClient({
